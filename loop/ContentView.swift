179f69437961955390b1d420d120ab838a5a4bd4
Aleksandar Ivanov <alexmmech@gmail.com>
2026-08-10 19:15:07 +0300

    Initial Commit

---
+++ loop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  loop
+//
+//  Created by Aleksander Ivanov on 10/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ loop/loopApp.swift
@@ -0,0 +1,17 @@
+//
+//  loopApp.swift
+//  loop
+//
+//  Created by Aleksander Ivanov on 10/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct loopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
