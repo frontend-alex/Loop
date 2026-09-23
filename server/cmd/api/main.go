1d36bda85724ae43e577e06eab6cfefc808c0dce
Aleksandar Ivanov <alexmmech@gmail.com>
2026-09-23 13:25:52 +0200

    feature: add initial server configuration and API implementation

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -14,13 +14,13 @@ type album struct {
 }
 
 var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "1", Title: "Blue Train", Artist: "Cock", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
 func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+	c.JSON(http.StatusOK, albums)
 }
 
 func main() {
